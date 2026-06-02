3d888704fa33b414a88eb379a429f86cf94a6250
Lucas Quintela <lucas@quintela.qa>
2026-06-02 15:02:23 -0300

    chore(posts): migrate article slug to CalVer with redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://lucasquintela23.github.io",
   base: "/blog-quintela/",
+  redirects: {
+    "/linha-de-producao-qa-vs-qc": "/2026-06-01-linha-de-producao-qa-vs-qc",
+  },
   integrations: [
     mdx(),
     tailwind({
